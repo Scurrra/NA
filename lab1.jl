786bd0e38cc71fa2b9491d2b6d0f1afa4c105838
Scurrra <iscurrra@gmail.com>
2022-02-15 22:13:25 +0300

    Initial commit

---
+++ lab1.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0cb92a82-8e93-11ec-091b-f1abb82c2225
+begin
+	import Pkg;
+	Pkg.activate()
+
+	using LinearAlgebra
+
+	using PlutoUI
+	TableOfContents()
+end
+
+# ╔═╡ Cell order:
+# ╠═0cb92a82-8e93-11ec-091b-f1abb82c2225
