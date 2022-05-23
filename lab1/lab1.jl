aea0bbe3fe515b145ebc3734f9fdd253b276e598
Scurrra <iscurrra@gmail.com>
2022-05-24 00:24:21 +0300

    updates

--- lab1/lab1.jl
+++ lab1/lab1.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.18.2
+# v0.19.5
 
 using Markdown
 using InteractiveUtils
@@ -15,6 +15,9 @@ begin
 	using PlutoUI
 	TableOfContents()
 end
+
+# ╔═╡ 6d05ed77-1924-4617-978b-c1fbfc0de550
+using ImplicitEquations, Plots
 
 # ╔═╡ 32f02311-d88b-47ef-a399-59752ce037a5
 md"""
@@ -45,20 +48,6 @@ md"""
 ## Задание 1. Метод Гаусса
 """
 
-# ╔═╡ f1857dc6-6a51-4bed-b2a0-749c325b7794
-D = [
-	6.22 1.42 -1.72 1.91
-	1.44 5.33 1.11 -1.82
-	-1.72 1.11 5.24 1.42
-	1.91 -1.82 1.42 6.55
-]
-
-# ╔═╡ 6bbb66c7-5458-4482-93e3-06293bf7dbb7
-A = D + I
-
-# ╔═╡ 53a63945-6809-4fc4-8987-21bd7b7c5e90
-f̄ = [7.53, 6.06, 8.05, 8.06]
-
 # ╔═╡ f2797a5d-8f42-4c03-ac10-4331c1075ff1
 begin
 	mutable struct SLE
@@ -81,7 +70,7 @@ begin
 
 	function solve(eq::SLE)
 		eq.det = 1
-		n = length(f̄)
+		n = length(eq.f̄)
 		eq.solution = DataFrame(
 			Matrix{Union{Missing, Number}}(missing, sum(2:n)+2n, 2+n+3),
 			[["Step", "i"]; [ "aᵢ$(i)" for i=1:(n+1) ]; ["Σ", "Σᵢ"] ]
@@ -139,11 +128,22 @@ begin
 	end
 end;
 
+# ╔═╡ f1857dc6-6a51-4bed-b2a0-749c325b7794
+D = [
+	6.22 1.42 -1.72 1.91
+	1.44 5.33 1.11 -1.82
+	-1.72 1.11 5.24 1.42
+	1.91 -1.82 1.42 6.55
+]
+
+# ╔═╡ 6bbb66c7-5458-4482-93e3-06293bf7dbb7
+A = D + I
+
+# ╔═╡ 53a63945-6809-4fc4-8987-21bd7b7c5e90
+f̄ = [7.53, 6.06, 8.05, 8.06]
+
 # ╔═╡ d052cb09-e039-4c65-96ad-e14c32a0fbbc
 sle = SLE(A, f̄)
-
-# ╔═╡ cd9d8f24-8bf7-49ee-a506-4a98c4c0a5d8
-sle |> solve
 
 # ╔═╡ cdab7141-216c-4f72-8c29-f2f5cd013670
 pretty_table(sle.solution, formatters=ft_nomissing)
@@ -229,6 +229,211 @@ md"""
 ## Задание 1. Нахождение обратной матрицы
 """
 
+# ╔═╡ e8ced749-64a9-4335-921c-e8e78764a14d
+sum(abs, A * inv(A) - I) 
+
+# ╔═╡ c4604f18-cc84-4a85-96d1-456423c6edb5
+md"""
+## Задание 2. Число обусловленности
+"""
+
+# ╔═╡ f5271f10-5515-46e6-ae62-cec7eec1b49e
+md"""
+# Лабораторная работа #3
+# Метод квадратных корней (м-д Холецкого)
+"""
+
+# ╔═╡ 2865dc6f-5c92-4752-88b8-5edb3e261e39
+A₃ = [
+	1.65 -1.76 0.77
+	-1.76 1.04 -2.61
+	0.77 -2.61 -3.18
+]
+
+# ╔═╡ 1db51949-df20-44eb-bbec-9047596a1284
+f̄₃ = [
+	2.15, 0.82, -0.73
+]
+
+# ╔═╡ 9f07676a-1777-42d5-aa04-68d1e3aebf86
+A₃ \ f̄₃
+
+# ╔═╡ 690da59c-dc1f-415f-89c4-9246216f1f41
+md"""
+## Задание 1. $A = Aᵀ > 0$
+"""
+
+# ╔═╡ b8ad41d5-88ee-4f75-9c54-5578501edf16
+A₃ᵀ = A₃ |> transpose |> Matrix
+
+# ╔═╡ d0e2df4d-df6a-4b2e-9843-58e9fcab2825
+A₃ᵀ == A₃
+
+# ╔═╡ 41776634-e2f1-427a-b516-ebd69d2293c4
+[
+	det(A₃[1:i, 1:i])
+	for i=1:size(A₃, 1)
+]
+
+# ╔═╡ 8c76b049-469c-4663-9b93-af04e1821e92
+ḡ₃ = A₃*f̄₃
+
+# ╔═╡ 76d93737-1712-4cdc-ab45-95c70eb752b1
+B = A₃ * A₃
+
+# ╔═╡ 0d33adbc-2cc6-4333-9765-ca5a6877a9a9
+[
+	det(B[1:i, 1:i])
+	for i=1:size(B, 1)
+]
+
+# ╔═╡ 65ec3210-63e4-45c6-bef6-e1bb45d3d16f
+md"""
+## Задание 2. $A = L * Lᵀ$
+"""
+
+# ╔═╡ 377c0a66-d2f7-445d-8425-ea5fa0d11bce
+function LLᵀ(A::Matrix)
+	L = zeros(size(A))
+	n = size(A, 1)
+	#return L
+
+	L[1, 1] = sqrt( A[1, 1] )
+	L[2:end, 1] .= A[2:end, 1] / L[1, 1]
+
+	#return L
+	
+	for k=2:n
+		L[k, k] = sqrt( A[k, k] - sum(a->a^2, L[k, 1:(k-1)]) )
+
+		for i=(k+1):n
+			L[i, k] = (A[i, k] - L[i, 1:(k-1)]' * L[k, 1:(k-1)]) / L[k, k]
+		end
+	end
+
+	L, L' |> Matrix
+end
+
+# ╔═╡ 38701eb2-8750-4d3f-add0-3d7f0c23d1f5
+L, Lᵀ = LLᵀ(B)
+
+# ╔═╡ 5bfe6216-29fb-41be-ba7a-22b8cbd8d1cb
+L * Lᵀ
+
+# ╔═╡ 94e4d42c-8ca3-4488-9959-bffe12088477
+B
+
+# ╔═╡ b5e0fa99-40de-4ee9-983d-ce4dce9e6dcc
+md"""
+## Задание 3. Обратный метод Гаусса
+"""
+
+# ╔═╡ 50da520d-5de8-4565-b411-9ad2de8a3131
+y₁ = ḡ₃[1] / L[1, 1]
+
+# ╔═╡ 5d374e90-202b-4d69-8e4a-f10328af3b30
+y₂ = (ḡ₃[2] - L[2, 1]*y₁) / L[2, 2]
+
+# ╔═╡ 6ebc8f74-254f-4211-a34a-9e1f1b2dd93e
+y₃ = (ḡ₃[3] - L[3, 1]*y₁ - L[3, 2]*y₂) / L[3, 3]
+
+# ╔═╡ 6d1fc1d3-e69e-47d1-933a-c14e74e52496
+x₃ = y₃ / Lᵀ[3, 3]
+
+# ╔═╡ f507f669-e56c-4588-80bb-c47e4076c1c4
+x₂ = (y₂ - Lᵀ[2, 3]*x₃) / Lᵀ[2, 2]
+
+# ╔═╡ 8c6a9db4-5509-47b2-8cf1-7857ca1dd6ed
+x₁ = (y₁ - Lᵀ[1, 3]*x₃ - Lᵀ[1, 2]*x₂) / Lᵀ[1, 1]
+
+# ╔═╡ 2bad0e5c-b1a7-47c1-b7e4-5824c4cb331c
+md"""
+## Задание 4. Проверка решения
+"""
+
+# ╔═╡ be9cdc8a-f563-4d5f-9037-cb7c13d5dd13
+f̄₃
+
+# ╔═╡ 6c4ccf17-99cb-4dab-89a8-3cdf10c049ea
+md"""
+# Лабораторная работа #4
+# Метод простых итераций
+
+## Задание 1
+
+1. Привести систему 
+$\begin{cases}
+	2x₁ + x₂ + αx₃ = -2.9 \\
+	αx₁+5x₂+0.72x₃=-0.7 \\
+	-1.2x₁ + 3x₂ + 1.7x₃ = -9.86
+\end{cases}\;, α = 0.1*K$
+
+к системе с диагональным преобладанием
+
+$K = 1$
+
+I + III - II -> III
+
+2 III + I - II -> III
+"""
+
+# ╔═╡ b166cf1b-d5d7-4b0d-b4ac-54967d3cd8a9
+α = 0.1
+
+# ╔═╡ 95fa8cbe-9c17-4277-a17a-a1299378a368
+A₄ = [
+	2 1 α -2.9
+	α 5 0.72 -0.7
+	-1.2 3 1.7 -9.86
+]
+
+# ╔═╡ 159b490d-a0ee-4094-8bbf-4f5ed6671e6e
+begin
+	A₄[3,:] = 2 .* A₄[3,:] .+ A₄[1,:] .- A₄[2,:]
+	A₄
+end
+
+# ╔═╡ 95aa6b37-dfcc-489f-8d9e-29dd424bbdf0
+f₄̄ = A₄[:, 4]
+
+# ╔═╡ f667d18f-a070-4921-9f6e-40a05abb13dc
+A₄̂ = A₄[:, 1:3]
+
+# ╔═╡ 9afaaf5d-e1a3-46f6-98dd-3e66f5bfa7d1
+ϵ = 1/2*10^-4
+
+# ╔═╡ 9615683c-bb4c-4d42-bbe6-5044ab2bedc7
+C = inv(diagm(diag(A₄̂)))
+
+# ╔═╡ 63083def-2687-4635-bf08-d82935c62b1f
+∞(I - C*A₄̂)
+
+# ╔═╡ dfa13584-a688-4ad4-b1d9-f8cdcc9c4727
+function solve(A::Matrix, f̄::Vector; ϵ=ϵ)
+	C = A |> diag |> diagm |> inv
+	B = I - C * A
+	ḡ = C * f̄
+	
+	x = [ḡ]
+	push!(x, B*ḡ + ḡ)
+
+	i = 2
+	while true
+		if ∞(B) ≤ 1//2
+			∞(x[i] - x[i-1]) ≤ ϵ && return B, ḡ, i, x
+		else
+			∞(x[i] - x[i-1]) ≤ (1-∞(B)) / ∞(B) * ϵ && return B, ḡ, i, x
+		end
+
+		push!(x, B*x[i] + ḡ)
+		i += 1
+	end
+	
+end
+
+# ╔═╡ cd9d8f24-8bf7-49ee-a506-4a98c4c0a5d8
+sle |> solve
+
 # ╔═╡ 073884d6-4126-4001-bb23-cc84b678cf7d
 map(
 	f -> begin
@@ -252,14 +457,6 @@ inv_sle(A::Matrix) = hcat(map(
 # ╔═╡ 5eea2411-64cc-40b9-a36b-3106f7ff26d0
 A * inv_sle(A)
 
-# ╔═╡ e8ced749-64a9-4335-921c-e8e78764a14d
-sum(abs, A * inv(A) - I) 
-
-# ╔═╡ c4604f18-cc84-4a85-96d1-456423c6edb5
-md"""
-## Задание 2. Число обусловленности
-"""
-
 # ╔═╡ a5fbe839-56cd-4338-be35-eded3046e3d9
 cond(A::Matrix; norm::Function=|₂) =
 	(norm(A), norm(inv_sle(A)), norm(A) * norm(inv_sle(A)))
@@ -273,14 +470,118 @@ cond(A, norm=|₁)
 # ╔═╡ 1977f686-9a9b-4e70-84b4-dd1dc394158b
 cond(A, norm=∞)
 
+# ╔═╡ a4b44707-15b9-4533-befe-4681f011c665
+ȳ = begin
+	sle₃₁ = SLE(L, ḡ₃)
+	sle₃₁ |> solve
+	sle₃₁.x
+end
+
+# ╔═╡ 8d7419f9-2885-426e-9709-fdceac7537e5
+sle₃₁.solution
+
+# ╔═╡ dbf0aaf2-9132-4ab4-b2ce-3ae85b1986f5
+ȳ
+
+# ╔═╡ 35928c65-a4b5-4c9f-8c05-886fa514a061
+x̄ = begin
+	sle₃₂ = SLE(Lᵀ, ȳ)
+	sle₃₂ |> solve
+	sle₃₂.x
+end
+
+# ╔═╡ 850a3b72-1f63-463e-8833-9256062711ff
+sle₃₂.solution
+
+# ╔═╡ 36446fa9-2848-4147-822a-81ff4acb16e9
+x̄
+
+# ╔═╡ 95cd2ca2-4e51-489a-b184-a726c7080b01
+A₃ * x̄ 
+
+# ╔═╡ 1e232704-cbd9-4d4f-9731-7bf7d04c37ee
+r̄ = A₃ * x̄ - f̄₃
+
+# ╔═╡ e48e96e2-ce80-436f-b065-6abf7c18eeff
+|₁(r̄)
+
+# ╔═╡ 4f2181c2-ccc9-42e9-b50f-c3a4f5d44267
+|₂(r̄)
+
+# ╔═╡ 890060d2-4307-4c0f-b2c2-d5711c1e8cc1
+∞(r̄)
+
+# ╔═╡ 9020a0ba-4320-45b8-b1ae-6dc256929528
+B̂, ḡ, it, x = solve(A₄̂, f₄̄)
+
+# ╔═╡ abc4a380-2b37-47a4-afed-f3122f142827
+B̂
+
+# ╔═╡ ee3a783f-b20b-4fdb-8752-9f3ca17462fa
+∞(B̂)
+
+# ╔═╡ d22af669-5481-4689-b68d-41db97c3d8c4
+∞(A₄̂ * x[end] - f₄̄)
+
+# ╔═╡ f12203e5-8d90-456d-8bc9-afa87880e920
+([
+	2 1 α
+	α 5 0.72
+	-1.2 3 1.7
+] * x[end] - [-2.9, -0.7, -9.86]) |> ∞
+
+# ╔═╡ 505969fb-57e7-4a92-9e27-2668f6b8d7ca
+A₄̂ \ f₄̄
+
+# ╔═╡ 05e30414-3e2b-4642-b685-5d03aa5b4b20
+md"""
+## Задание 2
+"""
+
+# ╔═╡ a5e5afd1-b39c-49bb-a7eb-2c8c8a51e9c3
+md"""
+$B = \begin{bmatrix}
+	p && q \\
+	q && p
+\end{bmatrix}$
+"""
+
+# ╔═╡ d5ee6675-2479-4f3e-9ba0-5240548ca69f
+NORM1(x, y) = ∞([x y; y x])
+
+# ╔═╡ 3fad14be-8441-43d6-b327-048cae944656
+plot(
+	NORM1 ≪ 1;
+	xlims=(-2, 2),
+	ylims=(-2, 2)
+)
+
+# ╔═╡ 11d3e4c6-6206-4b3b-a9d4-6c002cfe0953
+md"""
+$B = \begin{bmatrix}
+	p && q \\
+	-q && p
+\end{bmatrix}$
+"""
+
+# ╔═╡ 1eafd2f4-9fa2-4dff-9bf3-031e11e907a6
+NORM2(x, y) = ∞([x y; -y x])
+
+# ╔═╡ 936fb5fd-b611-4998-a813-0b17ceb39251
+plot(
+	NORM2 ≪ 1;
+	xlims=(-2, 2),
+	ylims=(-2, 2)
+)
+
 # ╔═╡ Cell order:
-# ╟─0cb92a82-8e93-11ec-091b-f1abb82c2225
+# ╠═0cb92a82-8e93-11ec-091b-f1abb82c2225
 # ╟─32f02311-d88b-47ef-a399-59752ce037a5
 # ╠═2fd3c08f-0768-4533-90be-d0f457f77bdf
 # ╠═90464d1e-75aa-423c-9f29-dce94cf84003
 # ╠═8d82ca3e-b058-49b7-8398-064de7da35d3
 # ╟─f3572ec8-4ec9-4b87-b39b-34f3f652c8a0
-# ╠═f2797a5d-8f42-4c03-ac10-4331c1075ff1
+# ╟─f2797a5d-8f42-4c03-ac10-4331c1075ff1
 # ╠═f1857dc6-6a51-4bed-b2a0-749c325b7794
 # ╠═6bbb66c7-5458-4482-93e3-06293bf7dbb7
 # ╠═53a63945-6809-4fc4-8987-21bd7b7c5e90
@@ -316,3 +617,63 @@ cond(A, norm=∞)
 # ╠═515370c4-8911-4aab-807a-89655aaaf341
 # ╠═b4d3a49d-368a-42db-afcc-4acce7c754eb
 # ╠═1977f686-9a9b-4e70-84b4-dd1dc394158b
+# ╟─f5271f10-5515-46e6-ae62-cec7eec1b49e
+# ╠═2865dc6f-5c92-4752-88b8-5edb3e261e39
+# ╠═1db51949-df20-44eb-bbec-9047596a1284
+# ╠═9f07676a-1777-42d5-aa04-68d1e3aebf86
+# ╟─690da59c-dc1f-415f-89c4-9246216f1f41
+# ╠═b8ad41d5-88ee-4f75-9c54-5578501edf16
+# ╠═d0e2df4d-df6a-4b2e-9843-58e9fcab2825
+# ╠═41776634-e2f1-427a-b516-ebd69d2293c4
+# ╠═8c76b049-469c-4663-9b93-af04e1821e92
+# ╠═76d93737-1712-4cdc-ab45-95c70eb752b1
+# ╠═0d33adbc-2cc6-4333-9765-ca5a6877a9a9
+# ╟─65ec3210-63e4-45c6-bef6-e1bb45d3d16f
+# ╠═377c0a66-d2f7-445d-8425-ea5fa0d11bce
+# ╠═38701eb2-8750-4d3f-add0-3d7f0c23d1f5
+# ╠═5bfe6216-29fb-41be-ba7a-22b8cbd8d1cb
+# ╠═94e4d42c-8ca3-4488-9959-bffe12088477
+# ╠═a4b44707-15b9-4533-befe-4681f011c665
+# ╠═35928c65-a4b5-4c9f-8c05-886fa514a061
+# ╟─b5e0fa99-40de-4ee9-983d-ce4dce9e6dcc
+# ╠═8d7419f9-2885-426e-9709-fdceac7537e5
+# ╠═dbf0aaf2-9132-4ab4-b2ce-3ae85b1986f5
+# ╠═50da520d-5de8-4565-b411-9ad2de8a3131
+# ╠═5d374e90-202b-4d69-8e4a-f10328af3b30
+# ╠═6ebc8f74-254f-4211-a34a-9e1f1b2dd93e
+# ╠═850a3b72-1f63-463e-8833-9256062711ff
+# ╠═36446fa9-2848-4147-822a-81ff4acb16e9
+# ╠═6d1fc1d3-e69e-47d1-933a-c14e74e52496
+# ╠═f507f669-e56c-4588-80bb-c47e4076c1c4
+# ╠═8c6a9db4-5509-47b2-8cf1-7857ca1dd6ed
+# ╟─2bad0e5c-b1a7-47c1-b7e4-5824c4cb331c
+# ╠═95cd2ca2-4e51-489a-b184-a726c7080b01
+# ╠═be9cdc8a-f563-4d5f-9037-cb7c13d5dd13
+# ╠═1e232704-cbd9-4d4f-9731-7bf7d04c37ee
+# ╠═e48e96e2-ce80-436f-b065-6abf7c18eeff
+# ╠═4f2181c2-ccc9-42e9-b50f-c3a4f5d44267
+# ╠═890060d2-4307-4c0f-b2c2-d5711c1e8cc1
+# ╟─6c4ccf17-99cb-4dab-89a8-3cdf10c049ea
+# ╠═b166cf1b-d5d7-4b0d-b4ac-54967d3cd8a9
+# ╠═95fa8cbe-9c17-4277-a17a-a1299378a368
+# ╠═159b490d-a0ee-4094-8bbf-4f5ed6671e6e
+# ╠═95aa6b37-dfcc-489f-8d9e-29dd424bbdf0
+# ╠═f667d18f-a070-4921-9f6e-40a05abb13dc
+# ╟─9afaaf5d-e1a3-46f6-98dd-3e66f5bfa7d1
+# ╠═9615683c-bb4c-4d42-bbe6-5044ab2bedc7
+# ╠═63083def-2687-4635-bf08-d82935c62b1f
+# ╠═dfa13584-a688-4ad4-b1d9-f8cdcc9c4727
+# ╠═9020a0ba-4320-45b8-b1ae-6dc256929528
+# ╠═abc4a380-2b37-47a4-afed-f3122f142827
+# ╠═ee3a783f-b20b-4fdb-8752-9f3ca17462fa
+# ╠═d22af669-5481-4689-b68d-41db97c3d8c4
+# ╠═f12203e5-8d90-456d-8bc9-afa87880e920
+# ╠═505969fb-57e7-4a92-9e27-2668f6b8d7ca
+# ╟─05e30414-3e2b-4642-b685-5d03aa5b4b20
+# ╠═6d05ed77-1924-4617-978b-c1fbfc0de550
+# ╟─a5e5afd1-b39c-49bb-a7eb-2c8c8a51e9c3
+# ╠═d5ee6675-2479-4f3e-9ba0-5240548ca69f
+# ╠═3fad14be-8441-43d6-b327-048cae944656
+# ╟─11d3e4c6-6206-4b3b-a9d4-6c002cfe0953
+# ╠═1eafd2f4-9fa2-4dff-9bf3-031e11e907a6
+# ╠═936fb5fd-b611-4998-a813-0b17ceb39251
---
+++ notebook.jl
@@ -0,0 +1,204 @@
+### A Pluto.jl notebook ###
+# v0.19.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e3ebbb44-daa3-11ec-36c2-a5272e97694d
+begin
+	import Pkg;
+	Pkg.activate()
+
+	using LinearAlgebra
+	using DataFrames, PrettyTables
+	using Polynomials
+
+	using PlutoUI
+	TableOfContents()
+end
+
+# ╔═╡ 002d16fa-9a2a-477d-bfd7-4324655183cd
+A = [
+	1 1.5 2.5 3.5
+	1.5 1 2 1.6
+	2.5 2 1 1.7
+	3.5 1.6 1.7 1
+]
+
+# ╔═╡ a83615c0-41b8-4fc3-afda-badc15241a86
+md"""
+## M₃
+"""
+
+# ╔═╡ 50139705-6ebb-465f-b92e-3c9629be0cf4
+begin
+	M₃ = diagm(ones(4))
+	M₃[3, :] = -A[4, :]
+	M₃[3, 3] = 1
+	M₃[3, :] = M₃[3, :] ./ A[4, 3]
+	M₃
+end
+
+# ╔═╡ 1ad77896-48da-4c0a-ac44-021012102c92
+begin
+	M₃⁻¹ = diagm(ones(4))
+	M₃⁻¹[3, :] = A[4, :]
+	M₃⁻¹
+end
+
+# ╔═╡ 4238440a-72f3-4a10-abd0-33df63bd24a9
+A¹ = M₃⁻¹ * A * M₃
+
+# ╔═╡ 65b0b1f4-995c-4f4f-a174-76a01f1860f4
+md"""
+## M₂
+"""
+
+# ╔═╡ c2209805-5b74-480b-a21e-a541a416ac05
+begin
+	M₂ = diagm(ones(4))
+	M₂[2, :] = -A¹[3, :]
+	M₂[2, 2] = 1
+	M₂[2, :] = M₂[2, :] ./ A¹[3, 2]
+	M₂
+end
+
+# ╔═╡ 5355e704-8267-4818-b23b-bc64ea09469f
+begin
+	M₂⁻¹ = diagm(ones(4))
+	M₂⁻¹[2, :] = A¹[3, :]
+	M₂⁻¹
+end
+
+# ╔═╡ 9bf294dd-0852-42d5-89b7-60297810158b
+A² = M₂⁻¹ * A¹ * M₂
+
+# ╔═╡ 1b024fb4-0711-4819-afec-0f4469f1dc29
+md"""
+## M₁
+"""
+
+# ╔═╡ 61f32ca4-f0b3-433b-9703-7031be7d6ec8
+begin
+	M₁ = diagm(ones(4))
+	M₁[1, :] = -A²[2, :]
+	M₁[1, 1] = 1
+	M₁[1, :] = M₁[1, :] ./ A²[2, 1]
+	M₁
+end
+
+# ╔═╡ 81b369b4-1d6b-49e1-83e4-2187dae242c1
+begin
+	M₁⁻¹ = diagm(ones(4))
+	M₁⁻¹[1, :] = A²[2, :]
+	M₁⁻¹
+end
+
+# ╔═╡ 25f66de9-086f-45a5-9332-6893fb40de39
+A³ = M₁⁻¹ * A² * M₁
+
+# ╔═╡ b66f37b1-961f-4e9e-b060-60297f9b1054
+md"""
+## S и S⁻¹
+"""
+
+# ╔═╡ ae70a894-0930-41e7-93ab-732da8d98397
+S = M₃ * M₂ * M₁ 
+
+# ╔═╡ 8f681374-2069-438d-8e60-53a41b8fe113
+S⁻¹ = M₁⁻¹ * M₂⁻¹ * M₃⁻¹
+
+# ╔═╡ 7f5ba1a7-46a2-41ff-9788-f533bdb11ba1
+S⁻¹ * A * S
+
+# ╔═╡ a56d6cdf-690d-433d-ad8a-d3a69b346b58
+eigen(A).values
+
+# ╔═╡ 6df3c70d-9a61-4629-bb6f-eaa499fc0c05
+eigen(S⁻¹ * A * S).values
+
+# ╔═╡ 9c4927c3-39de-4a21-ae87-856b19065f05
+md"""
+## Polynom
+"""
+
+# ╔═╡ 3f31618d-03c6-481b-be95-1fd8aa16532f
+Polynomial([1; -A³[1, :]] |> reverse)
+
+# ╔═╡ 8f85569c-269f-4b40-aef5-0c475c6b33e3
+λ = Polynomial([1; -A³[1, :]] |> reverse) |> roots
+
+# ╔═╡ c4703c87-c92d-4f0e-a37e-d1bc61e54407
+ȳ(i) = [λ[i]^3, λ[i]^2, λ[i], 1]
+
+# ╔═╡ daf71c2a-22a0-48bc-9dcc-29e1fbfbfdfe
+ȳ(1)
+
+# ╔═╡ 0e3ed9f8-b076-411a-a00e-ec091ad1ab78
+x̄(i) = M₃ * M₂ * M₁ * ȳ(i)
+
+# ╔═╡ e826069b-d16e-4d25-9f8e-0699e63694c6
+M₁ * ȳ(1)
+
+# ╔═╡ 12de2b7e-8d06-4e26-9fb1-eb325bda4b98
+M₂ * M₁ * ȳ(1)
+
+# ╔═╡ 6bead8fc-a2d0-459f-8bae-5d86a8408fe2
+M₃ * M₂ * M₁ * ȳ(1)
+
+# ╔═╡ d9c818e7-f7d1-407b-9ce4-e3fb2bf90bb5
+x̄(1)
+
+# ╔═╡ f995d12e-07fb-4d5e-949b-e3bbc40dccb4
+md"""
+## Check
+"""
+
+# ╔═╡ 433da834-cd3f-445e-bd9b-d4892e0db014
+S⁻¹ * A * S * ȳ(1), λ[1] * ȳ(1)
+
+# ╔═╡ 4899aada-9b4c-4e57-8de3-38dda92fa7f0
+A * x̄(1), λ[1] * x̄(1)
+
+# ╔═╡ 25d1cc43-8c2e-4fd1-92fc-1e3054ec8902
+
+
+# ╔═╡ e4d571d4-1c4d-4011-91b3-13003da57141
+
+
+# ╔═╡ Cell order:
+# ╟─e3ebbb44-daa3-11ec-36c2-a5272e97694d
+# ╟─002d16fa-9a2a-477d-bfd7-4324655183cd
+# ╟─a83615c0-41b8-4fc3-afda-badc15241a86
+# ╠═50139705-6ebb-465f-b92e-3c9629be0cf4
+# ╠═1ad77896-48da-4c0a-ac44-021012102c92
+# ╠═4238440a-72f3-4a10-abd0-33df63bd24a9
+# ╟─65b0b1f4-995c-4f4f-a174-76a01f1860f4
+# ╠═c2209805-5b74-480b-a21e-a541a416ac05
+# ╠═5355e704-8267-4818-b23b-bc64ea09469f
+# ╠═9bf294dd-0852-42d5-89b7-60297810158b
+# ╟─1b024fb4-0711-4819-afec-0f4469f1dc29
+# ╠═61f32ca4-f0b3-433b-9703-7031be7d6ec8
+# ╠═81b369b4-1d6b-49e1-83e4-2187dae242c1
+# ╠═25f66de9-086f-45a5-9332-6893fb40de39
+# ╟─b66f37b1-961f-4e9e-b060-60297f9b1054
+# ╠═ae70a894-0930-41e7-93ab-732da8d98397
+# ╠═8f681374-2069-438d-8e60-53a41b8fe113
+# ╠═7f5ba1a7-46a2-41ff-9788-f533bdb11ba1
+# ╠═a56d6cdf-690d-433d-ad8a-d3a69b346b58
+# ╠═6df3c70d-9a61-4629-bb6f-eaa499fc0c05
+# ╟─9c4927c3-39de-4a21-ae87-856b19065f05
+# ╠═3f31618d-03c6-481b-be95-1fd8aa16532f
+# ╠═8f85569c-269f-4b40-aef5-0c475c6b33e3
+# ╠═c4703c87-c92d-4f0e-a37e-d1bc61e54407
+# ╠═daf71c2a-22a0-48bc-9dcc-29e1fbfbfdfe
+# ╠═0e3ed9f8-b076-411a-a00e-ec091ad1ab78
+# ╠═e826069b-d16e-4d25-9f8e-0699e63694c6
+# ╠═12de2b7e-8d06-4e26-9fb1-eb325bda4b98
+# ╠═6bead8fc-a2d0-459f-8bae-5d86a8408fe2
+# ╠═d9c818e7-f7d1-407b-9ce4-e3fb2bf90bb5
+# ╟─f995d12e-07fb-4d5e-949b-e3bbc40dccb4
+# ╠═433da834-cd3f-445e-bd9b-d4892e0db014
+# ╠═4899aada-9b4c-4e57-8de3-38dda92fa7f0
+# ╠═25d1cc43-8c2e-4fd1-92fc-1e3054ec8902
+# ╠═e4d571d4-1c4d-4011-91b3-13003da57141
